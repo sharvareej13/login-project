0d805b629bb4075bdb5de02195db857c560713c4
Jadhav <sjad@softwareag.com>
2021-03-24 21:46:01 +0530

    new files tried adding

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 require('rootpath')();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const jwt = require('main/jwt');
+const errorHandler = require('main/errors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/user', require('./user/user.controller'));
+
+// global error handler
+app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 6000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
 });
